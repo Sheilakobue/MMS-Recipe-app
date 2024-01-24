1d00ec5faca4c51364c1f053c57846fc923a1134
Sheila Kobue <125880665+Sheilakobue@users.noreply.github.com>
2024-01-24 20:20:48 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,7 +2,10 @@
 export default function Home() {
   return (
     <section className="w-full flex-center flex-col">
-      <h1></h1>
+      <h1 className="head_text text-center orange_gradient">
+        Explore Food From Around the World!
+        <br className="max-md:hidden"/>
+      </h1>
       </section>
       
   )
