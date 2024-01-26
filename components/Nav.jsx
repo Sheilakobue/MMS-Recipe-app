8b5a9c4b251fb9109fae2905ed81e7897f817882
Sheila Kobue <125880665+Sheilakobue@users.noreply.github.com>
2024-01-26 14:09:39 +0200

    Update Nav.jsx

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,10 +6,22 @@ import { React, useState, useEffect } from 'react';
 import {signIn, signOut, useSession, getProviders} from 'next-auth/react';
 
 export default function Nav() {
+  const isUserLoggedIn = true;
+  const [providers, setProviders] = useState(null);
+
+  useEffect(()=> {
+    const setProviders =async()=>{
+      const response = await getProviders();
+      setProviders(response);
+    }
+    setProviders();
+  },[])
+
   return (
     <nav className='flex-between w-full mb-16 pt-3'>
       <Link href='/'className='flex gap-2 flex-center'>
-      <Image src='/public/assets/images/INTERNET CAFE LOGO.jpg'
+      <Image 
+      src='/assets/images/INTERNET CAFE LOGO.jpg'
       alt ='MMS Logo'
       width={30}
       height={30}
@@ -18,7 +30,47 @@ export default function Nav() {
       <p className='logo_text'>MMS-Dynamic</p>
       </Link>
 
-      {/*Mobile*/}
+      {/*Desktop Navigation*/}
+      <div className='sm:flex hidden'>
+        {isUserLoggedIn ? (
+          <div className='flex gap-3 md:gap-5'>
+            <Link href='/create-prompt' 
+            className='black_btn'>
+              Create Recipe
+            </Link>
+
+            <button type='button' 
+            onClick={signOut} 
+            className='outline_btn'> 
+            SignOut
+            </button>
+
+            <Link href='/profile'>
+              <Image
+              src='/assets/images/INTERNET CAFE LOGO.jpg'
+              width={37}
+              height={37}
+              className='rounded-full'
+              alt='profile'
+              />
+            </Link>
+            </div>
+        ):(
+          <>
+            {providers && 
+            object.values(providers).map((provider) =>(
+              <button 
+              type= 'button'
+              key={provider.name}
+              onClick={()=>signIn(provider.id)}
+              className='black_btn'
+              >
+                Sign In
+              </button>
+            ))}
+          </>
+          )}
+      </div>
     </nav>
   )
 }
