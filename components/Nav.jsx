8128ec7fe13af0a4615d85bc7f5bd1bfd2ff4c78
Sheila Kobue <125880665+Sheilakobue@users.noreply.github.com>
2024-01-26 10:22:09 +0200

    added a logo

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -9,13 +9,16 @@ export default function Nav() {
   return (
     <nav className='flex-between w-full mb-16 pt-3'>
       <Link href='/'className='flex gap-2 flex-center'>
-      <Image src='/public/assets/images/logo.svg'
+      <Image src='/public/assets/images/INTERNET CAFE LOGO.jpg'
       alt ='MMS Logo'
       width={30}
       height={30}
       className='object-contain'
       />
+      <p className='logo_text'>MMS-Dynamic</p>
       </Link>
+
+      {/*Mobile*/}
     </nav>
   )
 }
